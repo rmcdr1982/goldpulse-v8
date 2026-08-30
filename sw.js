504ec9a1bcb5f99acae1c5bdc2df71c3b176df11
rmcdr1982 <rmcdr1982@gmail.com>
2026-08-30 10:00:54 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const C='goldpulse-v8';const A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(new URL(e.request.url).origin===location.origin)e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{let q=r.clone();caches.open(C).then(c=>c.put(e.request,q));return r})))});
