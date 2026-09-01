cac92fc94a98259f179731050fa5e927657f3815
rmcdr1982 <rmcdr1982@gmail.com>
2026-09-01 08:09:27 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='goldpulse-v10-1';const A=['./','./index.html','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request))));
